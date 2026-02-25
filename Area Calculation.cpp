9fb5fed2d922b1d5032cba463ef199494f043377
HAEQAH FURQAN <furqanhaeqah@gmail.com>
2026-02-25 20:35:26 +0500

    Area Calculation.cpp

--- Area Calculation.cpp
+++ Area Calculation.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 main(){
-int A;
-int l;
-int w;
+float A;
+float l;
+float w;
 cout<<"Enter the length of the rectangle: ";
 cin>>l;
 cout<<"Enter the width of the rectangle: ";
 cin>>w;
 A = l*w;
 cout<<"The area of the rectangle is: "<<A;
+
 }
