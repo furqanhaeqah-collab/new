36ff71726adde7cbfda551e1f9f027bee8d92480
HAEQAH FURQAN <furqanhaeqah@gmail.com>
2026-02-27 20:23:00 +0500

    t

---
+++ Food_application.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main(){
+system("color 07");
+cout<<"                   *      *      *            "<<endl;
+cout<<"                     *      *      *          "<<endl;
+cout<<"                      *      *      *         "<<endl;
+cout<<"                     *      *      *          "<<endl;
+cout<<"                    *      *      *           "<<endl;
+cout<<"                     *      *      *          "<<endl;
+cout<<"                       *      *      *        "<<endl;
+cout<<"          ************************************"<<endl;
+cout<<"           *                                * "<<endl;
+cout<<"             *                            *   "<<endl;
+cout<<"               *                        *     "<<endl;
+cout<<"                 *                    *       "<<endl;
+cout<<"                   ******************         "<<endl<<endl;
+cout<<"              #WELCOME TO FOODY APPLICATION#  ";
+}
---
+++ Study.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+main(){
+cout<<"Programming Fundamentals Lab."<<endl<<"Welcome to the Lab";
+}
